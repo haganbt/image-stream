1292e1ebf43f348ee8985771c2231f3a2e9dbdcc
Ben Hagan <benhagan@gmail.com>
2013-06-29 09:02:05 +0100

    Formatting.

--- server.js
+++ server.js
@@ -27,11 +27,15 @@ app.post('/data', function (req, res) {
   		//var body = req.body.toString().split("\n");
   		var body = req.body;
 
+		// Each interaction
   		for(i in body) {
+  				
 			for(var opengraphIndx in body[i].links.meta.opengraph){ 	
-		 		socket.emit('ping', { msg: '<p><img src="' + body[i].links.meta.opengraph[opengraphIndx].image + '" /></p>' });
-			}	
+	 			socket.emit('ping', { msg: '<p><img src="' + body[i].links.meta.opengraph[opengraphIndx].image + '" /><br /><br />' + body[i].interaction.content  + '</p><br /><br />' });
+			}
+	
 		}
+
 
   } catch (e){
   	console.log('DEBUG: ' + e)
